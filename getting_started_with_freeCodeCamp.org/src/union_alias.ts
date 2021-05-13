c99afd61f47ff3cc3836b7c1afe265244c872389
Louis Leonardo <leonardolouis2@gmail.com>
2021-05-13 14:49:05 +0700

    init compiled ts

--- getting_started_with_freeCodeCamp.org/src/union_alias.ts
+++ getting_started_with_freeCodeCamp.org/src/union_alias.ts
@@ -2,7 +2,7 @@ type ID = string
 type PopularTag = string
 type MaybePopularTag = PopularTag | null
 
-interface UserInterface {
+interface UserInterface2 {
     name: string,
     surname: string
 }
@@ -17,6 +17,6 @@ let pagename: string | number = "1"
 
 let errMsg:string | null = null
 
-let newUser: UserInterface | null = null
+let newUser: UserInterface2 | null = null
 
 let someProp : string | number | null | undefined | string[] | object
