6f0fe749a436ae7a99991147f8b3cce3fd2ae14a
Louis Leonardo <leonardolouis2@gmail.com>
2021-05-14 14:03:21 +0700

    how to use generic interface and functions

---
+++ getting_started_with_freeCodeCamp.org/src/genericsInterfaceAndFunctions.ts
@@ -0,0 +1,35 @@
+// this is called generic functions to identify that this is an object by using <T> actually you can create any generic names you want, it captures all information, but the problem is that, it will allow any type so we need to use extends object in <> or you can custom it like below. 
+
+// const addId = <type extends {name:number}>(obj: type) => {
+const addId = <T extends object>(obj: T) => {
+    const id = Math.random().toString(16)
+    return {
+        ...obj,
+        id,
+    }
+}
+
+interface UserInterface123<T>{
+    name: string
+    data?: T
+}
+
+const user123: UserInterface123<{meta: string}> = {
+    name : "jake",
+    data: {
+        meta:'foo'
+    }
+}
+
+const user10: UserInterface123<string[]> = {
+    name:"john",
+    data:['lll','llll']
+}
+
+const user5451: UserInterface123<null> = {
+    name : 'joke'
+}
+
+const result = addId({name:'jake',})
+console.log(result.id)
+
